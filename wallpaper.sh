1c4f605f89deb3316c5fe53e312c9dc481fa641e
Michael <abebaw.michael@gmail.com>
2016-10-08 17:13:30 +0200

    first version

---
+++ wallpaper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Set the timer (s for Seconds, m for Minute, h for Hour)
+timer=5s
+#Set wallpaper directory path
+path="/home/michael/Documents/Wallpapers"
+
+#Loop through the wallpapers
+while :
+do
+	url=$(ls $path/* | shuf -n 1)
+	gsettings set org.gnome.desktop.background picture-uri "file://$url"
+	sleep $timer
+done
